32aaa52788759e3808bbbbdbbdf5dd9f3fae6e34
xlpn0423 <xlpn0423@leeds.ac.uk>
2026-01-31 15:32:53 +0000

    1

--- session_2/2_joins/tasks.sql
+++ session_2/2_joins/tasks.sql
@@ -7,3 +7,4 @@
 -- 2. Load this script: .read tasks.sql
 -- 3. Exit SQLite: .exit
 
+SELECT F1.username,users.username FROM users AS F1 JOIN follows ON follows.following_user_id=F1.id JOIN users ON followed_user_id=users.id;
