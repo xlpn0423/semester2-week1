8bb9a40da75cba9ea944dfa53cc17b789e32d67e
xlpn0423 <xlpn0423@leeds.ac.uk>
2026-01-31 18:15:01 +0000

    4

--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,3 +1,4 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
 -- DepartmentName, TotalEnrolments
+SELECT DepartmentName,COUNT(*) AS TotalEnrolments FROM Department GROUP BY DepartmentName;
