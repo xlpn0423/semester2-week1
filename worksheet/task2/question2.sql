566a4a2cfa6565634c1deee177b5d1a0c1e47e0c
xlpn0423 <xlpn0423@leeds.ac.uk>
2026-02-02 14:48:33 +0000

    7

--- worksheet/task2/question2.sql
+++ worksheet/task2/question2.sql
@@ -1,9 +1,4 @@
 -- Find the 5 youngest students in the database.
 -- Expected Columns:
-500|Christopher|Green|1995-01-08
-307|Dennis|Wilson|1995-01-11
-24|Sandra|Doyle|1995-01-18
-438|Jeffrey|Hopkins|1995-02-06
-88|Amber|Dean|1995-02-07
 -- StudentId, FirstName, LastName, DateOfBirth
 SELECT StudentId, FirstName, LastName, DateOfBirth FROM student ORDER BY DateOfBirth ASC LIMIT 5;
--- worksheet/task2/question3.sql
+++ worksheet/task2/question3.sql
@@ -1,9 +1,4 @@
 -- For each department, calculate the number of enrolments in its courses.
 -- Expected Columns:
-Business|635
-Computer Science|926
-History|496
-Mathematics|469
-Psychology|474
 -- DepartmentName, TotalEnrolments
 SELECT DepartmentName,COUNT(*) AS TotalEnrolments FROM Department JOIN Course ON Course.DepartmentId=Department.DepartmentId JOIN Enrolment ON Enrolment.CourseId=Course.CourseId GROUP BY DepartmentName;
