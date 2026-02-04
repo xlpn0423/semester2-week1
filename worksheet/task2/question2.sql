e2816c739a54190308308938fb11e4beaa9b7f30
xlpn0423 <xlpn0423@leeds.ac.uk>
2026-02-04 17:23:12 +0000

    11

--- worksheet/task2/question2.sql
+++ worksheet/task2/question2.sql
@@ -1,4 +1,4 @@
 -- Find the 5 youngest students in the database.
 -- Expected Columns:
 -- StudentId, FirstName, LastName, DateOfBirth
-SELECT StudentId, FirstName, LastName, DateOfBirth FROM student ORDER BY DateOfBirth ASC LIMIT 5;
+SELECT StudentId, FirstName, LastName, DateOfBirth FROM student ORDER BY DateOfBirth DESC LIMIT 5;
